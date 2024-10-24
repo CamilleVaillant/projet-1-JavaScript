a0612436516382774a390738f0e6c04f62989152
Camille Vaillant <cp.vaillant@gmail.com>
2024-10-24 16:42:03 +0200

    2emecommit

---
+++ assets/javascript/exo2-2.js
@@ -0,0 +1,16 @@
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+
+
+console.log(isEven(4)); /*Doit afficher true*/
+console.log(isEven(7)); /*Doit afficher false*/
+
+
+function isEven(number) {
+    return number % 2 === 0;
+}
